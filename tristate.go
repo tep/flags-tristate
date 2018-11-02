a13e15667bf0994d04d234abc89da8d83ef4d934
Tim Peoples <tim@toolman.org>
2018-11-02 13:09:12 -0700

    Start

---
+++ tristate.go
@@ -0,0 +1,185 @@
+//
+// Copyright 2018 Timothy E. Peoples
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+//
+
+// Package tristate provides a custom TriState flag for use with the alternate
+// flag package github.com/spf13/pflag. A TriState value may take one of three
+// forms: True, False or None and is most useful when you are, for instance,
+// filtering records based on a current boolean value -- and you need all
+// three possibilities (e.g. True, False and I Don't Care)
+//
+// There are 8 separate functions provided for defining a TriState flag with
+// some combination of the suffixes: "Var", "P", and "FS" each having the
+// following meaning:
+//
+// 		Var: Accepts a TriState pointer instead of returning one.
+// 		P:   Also takes a shorthand character for use with a single dash
+// 		FS:  Accepts a *FlagSet where the flag should be added
+//
+// The "Var" and "P" suffixes follow the common pflag convention. The "FS"
+// suffix is added to allow the use of alternate FlagSets.
+//
+package tristate
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/spf13/pflag"
+)
+
+var (
+	// CommandLine is the default FlagSet where flags will be added (unless
+	// otherwise specified)
+	CommandLine = pflag.CommandLine
+)
+
+// Flag defines a tristate.TriState flag with the specified name, default
+// value and usage string. The return value is the address of a TriState variable
+// the stores the values of the flag.
+func Flag(name string, value TriState, usage string) *TriState {
+	return FlagFS(CommandLine, name, value, usage)
+}
+
+// FlagP is similar to Flag butl also accepts a shorthand letter to be
+// used after a single dash.
+func FlagP(name, shorthand string, value TriState, usage string) *TriState {
+	return FlagPFS(CommandLine, name, shorthand, value, usage)
+}
+
+// FlagVar is similar to Flag that also accepts a pointer to TriState
+// variable where the flag value should be stored.
+func FlagVar(ts *TriState, name string, value TriState, usage string) {
+	FlagVarFS(CommandLine, ts, name, value, usage)
+}
+
+// FlagVarP is the combination of FlagVar and FlagP.
+func FlagVarP(ts *TriState, name, shorthand string, value TriState, usage string) {
+	FlagVarPFS(CommandLine, ts, name, shorthand, value, usage)
+}
+
+// FlagFS is similar to Flag but accepts a pointer to the FlagSet where
+// this flag should be added.
+func FlagFS(fs *pflag.FlagSet, name string, value TriState, usage string) *TriState {
+	return FlagPFS(fs, name, "", value, usage)
+}
+
+// FlagPFS is similar to FlagP but accepts a pointer to the FlagSet
+// where this flag should be added.
+func FlagPFS(fs *pflag.FlagSet, name, shorthand string, value TriState, usage string) *TriState {
+	ts := new(TriState)
+	FlagVarPFS(fs, ts, name, shorthand, value, usage)
+	return ts
+}
+
+// FlagVarFS is similar to FlagVar but accepts a pointer to the FlagSet
+// where this flag should be added.
+func FlagVarFS(fs *pflag.FlagSet, ts *TriState, name string, value TriState, usage string) {
+	FlagVarPFS(fs, ts, name, "", value, usage)
+}
+
+// FlagVarPFS is similar to FlagVarP but accepts a pointer to the FlagSet where this flag should be added.
+func FlagVarPFS(fs *pflag.FlagSet, ts *TriState, name, shorthand string, value TriState, usage string) {
+	fs.VarP(newTriState(value, ts), name, shorthand, usage)
+}
+
+// TriState is a TriState Value and may have one of three values: None, False or
+// True. Its "zero" value is None.
+type TriState int
+
+// The three possible tristate values
+const (
+	None TriState = iota
+	False
+	True
+)
+
+func newTriState(val TriState, p *TriState) *TriState {
+	*p = val
+	return (*TriState)(p)
+}
+
+// Get implements flag.Getter
+func (ts *TriState) Get() interface{} {
+	return TriState(*ts)
+}
+
+// String contributes to the implementation of pflag.Value
+func (ts TriState) String() string {
+	var s string
+	switch ts {
+	case None:
+		s = "None"
+	case False:
+		s = "False"
+	case True:
+		s = "True"
+	}
+	return s
+}
+
+// Type contributes to the implementation of pflag.Value
+func (ts *TriState) Type() string {
+	return "TriState"
+}
+
+// ErrBadTriStateValue is returned by Set if if cannot parse its input.
+var ErrBadTriStateValue = errors.New("bad tristate value")
+
+// Set the tristate.Value by parsing s according to the following rules:
+//
+//     Value: Strings
+//     ------ ----------------------------------------------------------
+//     True:  1, t, true, y, yes
+//     False: 0, f, false, n, no
+//     None:  -1, u, unknown, e, either, b, both, a, all, none, null, nil
+//
+// Any other string will return ErrBadTriStateValue
+//
+// Set contributes to the implementation of pflag.Value
+func (ts *TriState) Set(s string) error {
+	switch strings.ToLower(s) {
+	case "1", "t", "y", "true", "yes":
+		*ts = True
+	case "0", "f", "n", "false", "no":
+		*ts = False
+	case "-1", "u", "e", "b", "a", "unknown", "either", "both", "all", "any", "none", "null", "nil":
+		*ts = None
+	default:
+		return ErrBadTriStateValue
+	}
+	return nil
+}
+
+// Bool returns a pointer to a bool holding the value of ts. If ts is None then
+// Bool returns nil.
+func (ts *TriState) Bool() *bool {
+	var b bool
+	switch *ts {
+	case None:
+		return nil
+	case False:
+		b = false
+	case True:
+		b = true
+	}
+	return &b
+}
---
+++ tristate_test.go
@@ -0,0 +1,174 @@
+// Copyright 2018 Timothy E. Peoples
+
+package tristate
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/spf13/pflag"
+)
+
+func TestSet(t *testing.T) {
+	tests := map[TriState][]string{
+		None:  []string{"-1", "A", "ALL", "ANY", "All", "Any", "B", "BOTH", "Both", "E", "EITHER", "Either", "NIL", "NONE", "NULL", "Nil", "None", "Null", "U", "UNKNOWN", "Unknown", "a", "all", "any", "b", "both", "e", "either", "nil", "none", "null", "u", "unknown"},
+		False: []string{"0", "F", "FALSE", "False", "N", "NO", "No", "f", "false", "n", "no"},
+		True:  []string{"1", "T", "TRUE", "True", "Y", "YES", "Yes", "t", "true", "y", "yes"},
+	}
+
+	for want, list := range tests {
+		for _, s := range list {
+			ts := new(TriState)
+			if err := ts.Set(s); err != nil || *ts != want {
+				t.Errorf("ts.Set(%q) == %v (*ts=%s); Wanted %v (*ts=%v)", s, err, ts, nil, want)
+			}
+		}
+	}
+
+	ts := new(TriState)
+	if err := ts.Set("doug"); err != ErrBadTriStateValue {
+		t.Errorf("ts.Set(%q) == %v; Wanted %v", "doug", err, ErrBadTriStateValue)
+	}
+}
+
+func TestString(t *testing.T) {
+	tests := map[TriState]string{
+		None:  "None",
+		False: "False",
+		True:  "True",
+	}
+
+	for ts, want := range tests {
+		if got := ts.String(); got != want {
+			t.Errorf("%#v.String() == %q; wanted %q", ts, got, want)
+		}
+
+		p := &ts
+
+		if got := p.String(); got != want {
+			t.Errorf("%#v.String() == %q; wanted %q", p, got, want)
+		}
+	}
+}
+
+type flagOptions struct {
+	dflt TriState
+	args string
+	want TriState
+	err  error
+}
+
+func TestFlags(t *testing.T) {
+	var (
+		flagopts  []*flagOptions
+		flagPopts []*flagOptions
+	)
+
+	for _, dflt := range []TriState{None, False, True} {
+		flagopts = append(flagopts, &flagOptions{dflt, "", dflt, nil})
+		for _, want := range []TriState{None, False, True} {
+			flagopts = append(flagopts, &flagOptions{dflt, fmt.Sprintf("--tristate=%s", want.String()), want, nil})
+			flagopts = append(flagopts, &flagOptions{dflt, fmt.Sprintf("--tristate %s", want.String()), want, nil})
+			flagPopts = append(flagPopts, &flagOptions{dflt, fmt.Sprintf("-t %s", want.String()), want, nil})
+		}
+	}
+
+	var tests []*testcase
+	for _, what := range []string{"Flag", "FlagP", "FlagVar", "FlagVarP", "FlagFS", "FlagPFS", "FlagVarFS", "FlagVarPFS"} {
+		for _, fo := range flagopts {
+			tests = append(tests, mkTestcase(what, fo.args, fo.dflt, fo.want, fo.err))
+		}
+		if strings.HasSuffix(what, "P") {
+			for _, fo := range flagPopts {
+				tests = append(tests, mkTestcase(what, fo.args, fo.dflt, fo.want, fo.err))
+			}
+		}
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, tc.test)
+	}
+}
+
+type testcase struct {
+	name  string
+	setup func() (*pflag.FlagSet, *TriState)
+	args  string
+	want  TriState
+	err   error
+}
+
+func (tc *testcase) test(t *testing.T) {
+	defer func(fs *pflag.FlagSet) { CommandLine = fs }(CommandLine)
+
+	fs, got := tc.setup()
+	if err := fs.Parse(strings.Fields(tc.args)); err != tc.err || *got != tc.want {
+		t.Errorf("%s: got %v (err=%v); wanted %v (err=%v)", t.Name(), *got, err, tc.want, tc.err)
+	}
+}
+
+func mkTestcase(what, args string, dflt, want TriState, err error) *testcase {
+	fs := pflag.NewFlagSet("test:"+what, pflag.ContinueOnError)
+
+	var setup func() (*pflag.FlagSet, *TriState)
+	switch what {
+	case "Flag":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			CommandLine = fs
+			ts := Flag("tristate", dflt, "tristate flag")
+			return fs, ts
+		}
+
+	case "FlagP":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			CommandLine = fs
+			ts := FlagP("tristate", "t", dflt, "tristate flag")
+			return fs, ts
+		}
+
+	case "FlagVar":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			CommandLine = fs
+			var ts TriState
+			FlagVar(&ts, "tristate", dflt, "tristate flag")
+			return fs, &ts
+		}
+
+	case "FlagVarP":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			CommandLine = fs
+			var ts TriState
+			FlagVarP(&ts, "tristate", "t", dflt, "tristate flag")
+			return fs, &ts
+		}
+
+	case "FlagFS":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			ts := FlagFS(fs, "tristate", dflt, "tristate flag")
+			return fs, ts
+		}
+
+	case "FlagPFS":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			ts := FlagPFS(fs, "tristate", "t", dflt, "tristate flag")
+			return fs, ts
+		}
+
+	case "FlagVarFS":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			var ts TriState
+			FlagVarFS(fs, &ts, "tristate", dflt, "tristate flag")
+			return fs, &ts
+		}
+
+	case "FlagVarPFS":
+		setup = func() (*pflag.FlagSet, *TriState) {
+			var ts TriState
+			FlagVarPFS(fs, &ts, "tristate", "t", dflt, "tristate flag")
+			return fs, &ts
+		}
+	}
+
+	return &testcase{fmt.Sprintf("F:%s+D:%s+W:%s+A:%s", what, dflt, want, args), setup, args, want, err}
+}
